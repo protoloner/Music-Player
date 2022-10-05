b4215016ad3f6a1c0ceb1aadd147476d96184c52
Kshitiz <kshitizranjangupta@gmail.com>
2022-10-06 02:02:54 +0530

    Added songs I love

--- script.js
+++ script.js
@@ -12,27 +12,164 @@ const nextBtn=document.getElementById('next');
 
 const songs=[
     {
-        name: 'jacinto-1',
-        displayName: "1:Electric",
-        artist: 'Arunachalam',
+        name: 'A thousand years',
+        displayName: "A Thousand Years",
+        artist: 'Christina Perri',
 
     },
     {
-        name: 'jacinto-2',
-        displayName: "2:Electri",
-        artist: 'Arunachalam',
+        name: 'Jaane wo kaise',
+        displayName: "Jaane Woh Kaise Log The",
+        artist: 'Hemant Kumar',
     },
 
     {
-        name: 'jacinto-3',
-        displayName: "3:Elecri",
-        artist: 'Arunachalam',
+        name: 'Apocalypse',
+        displayName: "Apocalypse-CAS",
+        artist: 'Greg Gonzalez',
     },
     {
-        name: 'jacinto-4',
-        displayName: "4:Eletri",
-        artist: 'Arunachalam',
-    }
+        name: 'Dreamgirl',
+        displayName: "Dreamgirl",
+        artist: 'Teenage Blues',
+    },
+    {
+        name: 'My heart',
+        displayName: "My Heart Will Go On",
+        artist: 'Celine Dion',
+    },
+    {
+        name: 'Everybody',
+        displayName: "Everybody Knows",
+        artist: 'Sigrid',
+    },
+    {
+        name: 'Beyond love',
+        displayName: "Beyond Love",
+        artist: 'Beach House',
+    },
+    {
+        name: 'Eleanor',
+        displayName: "Eleanor And Park",
+        artist: 'Girl In Red , Beabadoobee',
+    },
+    {
+        name: 'Never fall in love',
+        displayName: "Until I found You",
+        artist: 'Stephen Sanchez , Em Beihold',
+    },
+    {
+        name: 'Walk',
+        displayName: "I Walk This Earth All By Myself",
+        artist: 'Ekkstacy',
+    },
+    {
+        name: 'Yuguure',
+        displayName: "Yuugure No Tori",
+        artist: 'Shinsei Kamattechan',
+    },
+    {
+        name: 'Something',
+        displayName: "Something In The Way",
+        artist: 'Nirvana-Kurt Cobain',
+    },
+    {
+        name: 'Amplifier',
+        displayName: "Amplifier",
+        artist: 'Imran Khan',
+    },
+    {
+        name: 'Tennu',
+        displayName: "Tennu Le",
+        artist: 'Ntedit , Omer Inayat',
+    },
+    {
+        name: 'Lost in paradise',
+        displayName: "Lost In Paradise",
+        artist: 'ALI feat. AKLO',
+    },
+   
+    {
+        name: 'Wellerman',
+        displayName: "Wellerman",
+        artist: 'Nathan Evans',
+    },
+    {
+        name: 'Rock',
+        displayName: "We Will Rock You",
+        artist: 'Queen',
+    },
+    {
+        name: 'Rasputin',
+        displayName: "Rasputin",
+        artist: 'Boney M.',
+    },
+    {
+        name: 'My mother',
+        displayName: "My Mother Told Me",
+        artist: 'Perly I Lotry',
+    },
+    {
+        name: 'My brother',
+        displayName: "My Brother Come Join Me",
+        artist: 'Kentaro Miura',
+    },
+    {
+        name: 'Rashmirathi',
+        displayName: "Rashmirathi",
+        artist: 'Ramdhari Singh Dinkar - Ashutosh Rana',
+    },
+    {
+        name: 'Thumak',
+        displayName: "Thumak Chalat Ramchandra",
+        artist: 'Maithili Thakur',
+    },
+    {
+        name: 'Awadh mein raam',
+        displayName: "Awadh Mein Raam Aaye Hain",
+        artist: 'Jaya Kishori',
+    },
+    {
+        name: 'Bade Badai',
+        displayName: "Bade Badai Na Kare",
+        artist: 'Rahim- Ankhiyo Ke Jharokon Se',
+    },
+    {
+        name: 'Sanchi kahein',
+        displayName: "Sanchi Kahein Tohre Aawan Se Bhauji",
+        artist: 'Jaspal Singh',
+    },
+    {
+        name: 'Kaune disha mein',
+        displayName: "Kaune Disha Me Le Ke Chala Re",
+        artist: 'Hemlata , Jaspal Singh',
+    },
+    {
+        name: 'Jug jug',
+        displayName: "Jug Jug Jiya Tu Lalanwa",
+        artist: 'Lokgeet',
+    },
+    {
+        name: 'Teri ungli',
+        displayName: "Teri Ungli Pakad Ke chala",
+        artist: 'Jyotsna Hardikar , Udit Narayan',
+    },
+    {
+        name: 'Hoshwalon',
+        displayName: "Hoshwalon Ko Khabar Kya",
+        artist: 'Jagjeet Singh',
+    },
+    {
+        name: 'Tauba',
+        displayName: "Tauba Tumhare Ye Ishaare",
+        artist: 'Abhijeet Bhattacharya',
+    },
+    {
+        name: 'Baalam',
+        displayName: "Balam Thanedar(Gypsy)",
+        artist: 'GD Kaur',
+    },
+
 ]
 
 //Check if playing or not
